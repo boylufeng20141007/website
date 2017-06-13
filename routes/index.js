c0930e0d092c370ee6c535e6d5e9e21a630e395f
Lux <boylufeng@gmail.com>
2017-06-13 23:48:12 +0800

    增加导航栏

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+/**
+ * 首页router
+ * @type {[type]}
+ */
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var renderData = {
+		title: '中山深惠智能科技设备有限公司'
+	};
+  res.render('index',renderData );
 });
 
 module.exports = router;
