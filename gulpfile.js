18744860829fe0bea9ca23341a37023306b45669
lux.lu <lux.lu@vipshop.com>
2017-06-14 18:33:17 +0800

    配置scss编译环境

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return gulp.src('./public/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./sourcemap'))
+        .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./public/scss/*.scss', ['sass'], function (e) {
+        console.log(e);
+    });
+});
+
+gulp.task('default', ['sass:watch'], function () {
+    //
+});
