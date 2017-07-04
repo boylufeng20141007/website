381af53eee88ccb06f72bc647c1702b025c367cf
lux.lu <lux.lu@vipshop.com>
2017-07-04 13:33:43 +0800

    增加vconsolo调试

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,15 @@ var swipeDao = require('../dao/swipe-dao');
 
 /* GET home page. */
 router.get(['/', '/index'], function (req, res/* , next */) {
+  var debug = req.query.debug;
   var data = {
     title: '中山深惠智能科技设备有限公司',
     head: {
       link: 'index'
-    }
+    },
+    debug: debug
   };
+  console.log(debug);
   res.render('page/index.html', data);
 });
 
